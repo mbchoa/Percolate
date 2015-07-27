544b40f6b92f953927e30cf6f0f59e8801ca7c63
Michael Choa <mbchoa@gmail.com>
2015-07-27 09:48:39 -0400

    Added class Javadoc comment Added print outs to evaluate mean, stddev and run time execution

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -2,6 +2,12 @@ public class PercolationStats {
     private double[] thresholdEstimates;
     private int numTests;
 
+    /**
+     * Initializes T percolation objects P and calculates the threshold estimate
+     * @param N the percolation grid size
+     * @param T the number of test cases
+     * @throws java.lang.IllegalArgumentException if N <= 0 or if T <= 0
+     */
     public PercolationStats(int N, int T) {
         if(N <= 0)  throw new IllegalArgumentException();
         if(T <= 0)  throw new IllegalArgumentException();
@@ -40,7 +46,13 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
-        PercolationStats pstats = new PercolationStats(200, 100);
-        StdOut.println("mean:\t" + pstats.mean());
+        Stopwatch watch = new Stopwatch();
+        PercolationStats pstats = new PercolationStats(20, 100);
+        double runtime = watch.elapsedTime();
+        StdOut.println("total runtime:\t" + runtime);
+        StdOut.println("mean:\t\t\t\t\t\t" + pstats.mean());
+        StdOut.println("stddev:\t\t\t\t\t\t" + pstats.stddev());
+        StdOut.println("95% confidence interval:\t" + pstats.confidenceLo() + ", " + pstats.confidenceHi());
+
     }
 }
