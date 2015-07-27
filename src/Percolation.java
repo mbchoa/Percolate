eb9382e0cb8659f11473242d0329166c6f8dad3b
Michael Choa <mbchoa@gmail.com>
2015-07-26 20:30:54 -0400

    Added Javadoc comments and formatted code Add PercolationVisualizer for testing

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,15 +1,30 @@
+/*********************************************************************************************
+ * Name: Michael-Bryant Choa
+ * Date: July 26, 2015
+ * Purpose:  Provides an object to model a 2D, N-by-N percolation system
+ * How to use:
+ *  1. Initialize Peroclation with parameter N, specifying the grid dimensions
+ *  2. Make calls to the open() method, specifying the position of the site to open
+ *  3. Call isOpen(), isFull() and percolates() methods to determine state of the system
+ *********************************************************************************************/
+
 public class Percolation {
     private class Site {
         int id;
         boolean open;
     }
 
-    private QuickFindUF uf;
-    private Site[][] grid;
-    private int top;
-    private int bottom;
-    private int size;
+    private QuickFindUF uf;     // union-find data structure to keep track of system connectivity
+    private Site[][] grid;      // 2D array data structure to keep to map system
+    private int top;            // virtual site connecting the top sites
+    private int bottom;         // virtual site connecting the bottom sites
+    private int size;           // size of grid
 
+    /**
+     * Initializes a 2D array data structure with N*N components and a QuickFindUF object
+     * @throws java.lang.IllegalArgumentException if N <= 0
+     * @param N the number of objects
+     */
     public Percolation(int N) {
         if (N <= 0) throw new IllegalArgumentException();
         size = N;
@@ -31,57 +46,64 @@ public class Percolation {
         }
     }
 
-    public void open(int i, int j) {
-        if (isBoundsOutOfRange(i, j))
+    /**
+     * Opens the percolation site at location (x, y) in the grid
+     * @param x the x coordinate of the site position
+     * @param y the y coordinate of the site position
+     */
+    public void open(int x, int y) {
+        if (isBoundsOutOfRange(x, y))
             throw new IndexOutOfBoundsException();
-        int gridI = i - 1;
-        int gridJ = j - 1;
+        int gridI = x - 1;
+        int gridJ = y - 1;
         grid[gridI][gridJ].open = true;
-        if (!isBoundsOutOfRange(i - 1, j) && isOpen(i - 1, j))
+        if (!isBoundsOutOfRange(x - 1, y) && isOpen(x - 1, y))
             uf.union(grid[gridI - 1][gridJ].id, grid[gridI][gridJ].id);
-        if (!isBoundsOutOfRange(i, j - 1) && isOpen(i, j - 1))
+        if (!isBoundsOutOfRange(x, y - 1) && isOpen(x, y - 1))
             uf.union(grid[gridI][gridJ - 1].id, grid[gridI][gridJ].id);
-        if (!isBoundsOutOfRange(i + 1, j) && isOpen(i + 1, j))
+        if (!isBoundsOutOfRange(x + 1, y) && isOpen(x + 1, y))
             uf.union(grid[gridI + 1][gridJ].id, grid[gridI][gridJ].id);
-        if (!isBoundsOutOfRange(i, j + 1) && isOpen(i, j + 1))
+        if (!isBoundsOutOfRange(x, y + 1) && isOpen(x, y + 1))
             uf.union(grid[gridI][gridJ + 1].id, grid[gridI][gridJ].id);
     }
 
-    public boolean isOpen(int i, int j) {
-        if (isBoundsOutOfRange(i, j))
+    /**
+     * Returns if the percolation site at (x, y) is open
+     * @param x the column position of site
+     * @param y the row position of the site
+     * @return true if the site at the position is open, false otherwise
+     */
+    public boolean isOpen(int x, int y) {
+        if (isBoundsOutOfRange(x, y))
             throw new IndexOutOfBoundsException();
-        return grid[i - 1][j - 1].open;
+        return grid[x - 1][y - 1].open;
     }
 
-    public boolean isFull(int i, int j) {
-        if (isBoundsOutOfRange(i, j))
+    /**
+     * Returns if the percolation site at (x, y) is filled
+     * @param x the column position of the site
+     * @param y the row position of the site
+     * @return true if the site at the position is full, false otherwise
+     */
+    public boolean isFull(int x, int y) {
+        if (isBoundsOutOfRange(x, y))
             throw new IndexOutOfBoundsException();
-        return grid[i - 1][j - 1].open && uf.connected(grid[i - 1][j - 1].id, top);
+        return grid[x - 1][y - 1].open && uf.connected(grid[x - 1][y - 1].id, top);
     }
 
-    private boolean isBoundsOutOfRange(int i, int j) {
-        return i < 1 || i >= size + 1 || j < 1 || j >= size + 1;
+    /**
+     * Returns if the site position is within bounds of percolation grid
+     * @param x the column position of the site
+     * @param y the row position of the site
+     * @return true if the position is within bounds of grid, false otherwise
+     */
+    private boolean isBoundsOutOfRange(int x, int y) {
+        return x < 1 || x >= size + 1 || y < 1 || y >= size + 1;
     }
 
-    public boolean percolates() {
-        return uf.connected(top, bottom);
-    }
-
-    /*
-    public static void main(String[] args) {
-        Percolation p = new Percolation(2);
-        p.open(1, 1);
-        StdOut.println(p.isFull(1, 1));
-        StdOut.println(p.isFull(1, 2));
-        StdOut.println(p.isFull(2, 1));
-        StdOut.println(p.isFull(2, 2));
-        StdOut.println("System percolates?:\t" + p.percolates());
-        p.open(2, 1);
-        StdOut.println(p.isFull(1, 1));
-        StdOut.println(p.isFull(1, 2));
-        StdOut.println(p.isFull(2, 1));
-        StdOut.println(p.isFull(2, 2));
-        StdOut.println("System percolates?:\t" + p.percolates());
-    }
-    */
+    /**
+     * Returns if the system percolates
+     * @return true if the system percolates, false otherwise
+     */
+    public boolean percolates() { return uf.connected(top, bottom);}
 }
